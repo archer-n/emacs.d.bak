6bafc8502447f8abd9bd6b51669c78b1b4d03f2c
archer <lazyarcher.wang@gmail.com>
2021-12-16 14:32:12 +0800

    The new org global shortcuts

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,10 +3,40 @@
 ;;; Commentary:
 ;;
 
+;; Among settings for many aspects of `org-mode', this code includes
+;; an opinionated setup for the Getting Things Done (GTD) system based
+;; around the Org Agenda.  I have an "inbox.org" file with a header
+;; including
+
+;;     #+CATEGORY: Inbox
+;;     #+FILETAGS: INBOX
+
+;; and then set this file as `org-default-notes-file'.  Captured org
+;; items will then go into this file with the file-level tag, and can
+;; be refiled to other locations as necessary.
+
+;; Those other locations are generally other org files, which should
+;; be added to `org-agenda-files-list' (along with "inbox.org" org).
+;; With that done, there's then an agenda view, accessible via the
+;; `org-agenda' command, which gives a convenient overview.
+;; `org-todo-keywords' is customised here to provide corresponding
+;; TODO states, which should make sense to GTD adherents.
+
+
 ;;; Code:
 
 (define-key global-map (kbd "C-c l") 'org-store-link)
 (define-key global-map (kbd "C-c a") 'org-agenda)
+
+(defvar archer/org-global-prefix-map (make-sparse-keymap)
+  "A keymap for handy global access to org helpers, particularly clocking.")
+
+(define-key archer/org-global-prefix-map (kbd "j") 'org-clock-goto)
+(define-key archer/org-global-prefix-map (kbd "l") 'org-clock-in-last)
+(define-key archer/org-global-prefix-map (kbd "i") 'org-clock-in)
+(define-key archer/org-global-prefix-map (kbd "o") 'org-clock-out)
+(define-key global-map (kbd "C-c o") archer/org-global-prefix-map)
+
 
 (setq org-startup-indented t
       org-src-tab-acts-natively t
