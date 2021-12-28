e7750d7a8cf9cb0b14e66ecc7a95bd845edb13f3
archer <lazyarcher.wang@gmail.com>
2021-12-28 13:47:24 +0800

    Update the RSS subscription list

--- lisp/init-rss.el
+++ lisp/init-rss.el
@@ -10,16 +10,32 @@
   (global-set-key (kbd "C-x w") 'elfeed)
   :custom
   (elfeed-curl-extra-arguments '("-x" "http://127.0.0.1:58591"))
-  (elfeed-feeds '(("http://nullprogram.com/feed/" blog emacs)
-                  "https://planet.emacslife.com/atom.xml"
-                  ("http://tech.meituan.com/atom.xml" meituan)
-                  ("http://feeds.feedburner.com/ruanyifeng" ruanyifeng)
-                  ("https://blog.lilydjwg.me/feed" lily)
-                  "https://inside.java/feed.xml"
-                  "https://hacks.mozilla.org/feed/"
-                  "https://hnrss.org/best"
-                  "https://kernel.taobao.org/feed.xml"
-                  "https://www.zhangxinxu.com/wordpress/feed/")))
+  (elfeed-feeds '(
+        ;; programming
+        ("https://news.ycombinator.com/rss" hacker)
+        ("https://www.heise.de/developer/rss/news-atom.xml" heise)
+        ("https://www.reddit.com/r/programming.rss" programming)
+        ("https://www.reddit.com/r/emacs.rss" emacs)
+
+        ;; programming languages
+        ("https://www.reddit.com/r/golang.rss" golang)
+        ("https://www.reddit.com/r/java.rss" java)
+        ("https://www.reddit.com/r/javascript.rss" javascript)
+        ("https://www.reddit.com/r/typescript.rss" typescript)
+        ("https://www.reddit.com/r/clojure.rss" clojure)
+        ("https://www.reddit.com/r/python.rss" python)
+
+        ;; cloud
+        ("https://www.reddit.com/r/aws.rss" aws)
+        ("https://www.reddit.com/r/googlecloud.rss" googlecloud)
+        ("https://www.reddit.com/r/azure.rss" azure)
+        ("https://www.reddit.com/r/devops.rss" devops)
+        ("https://www.reddit.com/r/kubernetes.rss" kubernetes)
+
+        ;; stats
+        ("http://www.stats.gov.cn/tjsj/zxfb/rss.xml" stats-zxfb)
+        ("http://www.stats.gov.cn/tjsj/sjjd/rss.xml" stats-sjjd)
+        )))
 
 (provide 'init-rss)
 ;;; init-rss.el ends here
